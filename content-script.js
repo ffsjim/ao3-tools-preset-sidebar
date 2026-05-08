900f4d2ad9580692e7ec600879f4d77091afa652
ffsjim <108143133+ffsjim@users.noreply.github.com>
2026-05-07 21:28:48 -0500

    Create content-script.js

---
+++ content-script.js
@@ -0,0 +1,9 @@
+// content-script.js
+
+(function() {
+  // 🔹 Paste your full, working sidebar code here.
+  // Make sure it starts with something like:
+  //   console.log("AO3 preset sidebar: main script starting");
+  //
+  // And ends with your form detection + buildSidebar() call.
+})();
